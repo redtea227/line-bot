ffa5492e1f014c61318039640bb59a1a3043520c
Roger Lo <redtea227@hotmail.com>
2020-12-16 17:27:53 +0800

    update

--- app.py
+++ app.py
@@ -37,9 +37,17 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '很抱歉,您說什麼'
+
+    if msg == 'hi':
+        r = 'hi'
+    elif msg == '你吃飯了嗎':
+        r = '還沒'
+        
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=))
 
 
 if __name__ == "__main__":
